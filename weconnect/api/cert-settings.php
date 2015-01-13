c7a22669c6fd0810062acef0951525d969244e73
Wahid eLoad <Wahid.eLoad@gmail.com>
2015-01-13 11:50:02 +0100

    use default /opt/certs path for certificates

--- weconnect/api/cert-settings.php
+++ weconnect/api/cert-settings.php
@@ -1,5 +1,5 @@
 <?php
   $weconnect_ssl_pass = "FIXME_KEY_PASS";
-  $weconnect_ssl_key  = "FIXME_CERTS_DIR".DIRECTORY_SEPARATOR."we-key.pem";
-  $weconnect_ssl_cert = "FIXME_CERTS_DIR".DIRECTORY_SEPARATOR."we-cert.pem";
+  $weconnect_ssl_key  = "/opt/certs/we-key.pem";
+  $weconnect_ssl_cert = "/opt/certs/we-cert.pem";
 ?>
