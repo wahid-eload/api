f3629193f6c8f1648d6f5e43899b2eaa8e1deb1b
Malik Shahzad Muzaffar <shahzad.malik.muzaffar@cern.ch>
2020-08-08 12:50:28 +0200

    Update settings.php

--- weconnect/api/settings.php
+++ weconnect/api/settings.php
@@ -4,8 +4,8 @@
   $weconnect_user     = 'FIXME_REGISTER_NUMBER';
   //This is your weconnect 4 to 6 digit PIN 
   $weconnect_pin      = 'FIXME_WAHID_PIN';
-  //Your API key, pleae get it from the weconnect.com.pk web sitr
+  //Your API key, pleae get it from the weconnect.com.pk web site
   $weconnect_apikey   = 'FIXME_APIKEY';
-  //Weconnect server. No need to change it unless WE ask your to do so.
+  //Weconnect server. No need to change it unless WE ask you to do so.
   $weconenct_host     = 'www.weconnect.com.pk';
 ?>
